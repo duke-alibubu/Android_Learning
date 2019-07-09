3ded1b7019d946a6f2eac5d770d8706f18c81e02
Trinh Dung <c170017@e.ntu.edu.sg>
2019-07-09 22:37:19 +0800

    Add simple listeners for color views

--- ColorMyViews/app/src/main/java/com/android/example/colormyviews/MainActivity.kt
+++ ColorMyViews/app/src/main/java/com/android/example/colormyviews/MainActivity.kt
@@ -1,12 +1,51 @@
 package com.android.example.colormyviews
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var boxOneText:TextView
+    private lateinit var boxTwoText:TextView
+    private lateinit var boxThreeText:TextView
+    private lateinit var boxFourText:TextView
+    private lateinit var boxFiveText:TextView
+    private lateinit var rootConstraintLayout:View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        boxOneText = findViewById(R.id.box_one_text)
+        boxTwoText = findViewById(R.id.box_two_text)
+        boxThreeText = findViewById(R.id.box_three_text)
+        boxFourText = findViewById(R.id.box_four_text)
+        boxFiveText = findViewById(R.id.box_five_text)
+        rootConstraintLayout = findViewById(R.id.constraint_layout)
+
+        setListeners()
+
+    }
+    
+    private fun makeColored(view: View){
+        when (view.id){
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
+            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
+    }
+
+    private fun setListeners(){
+        val clickableViews: List<View> = listOf(boxOneText, boxTwoText, boxThreeText, boxFourText, boxFiveText, rootConstraintLayout)
+        for (item in clickableViews)
+        {
+            item.setOnClickListener{makeColored(it)}
+        }
     }
 }
